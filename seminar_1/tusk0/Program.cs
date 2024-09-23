cc89c0491bd41fda707100b01b98d450d4a46727
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 13:36:57 +0400

    create program tusk0

--- seminar_1/tusk0/Program.cs
+++ seminar_1/tusk0/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да
+// a = -3, b = 9 => нет
+
+int num1;
+int num2;
+
+Console.Write("Введите первое число:");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Второе число:");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2*num2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
