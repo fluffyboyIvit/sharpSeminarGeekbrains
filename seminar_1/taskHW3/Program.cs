b82cbc65c0727bb332fab4d5cf29dddd4ab35c93
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 19:14:14 +0400

    create and wrote taskHW3 and add gitignore

---
+++ seminar_1/taskHW3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Определение, является ли число положительным
+// Описание: Напишите метод, который на вход принимает число и выводит, является ли
+// оно положительным (больше нуля), отрицательным (меньше нуля) или нулём.
+
+
+class Answer {
+static void CheckNumber(int number)
+{
+// Введите свое решение ниже
+if(number < 0)
+{
+    Console.WriteLine("отрицательное");
+}
+else if(number == 0)
+{
+    Console.WriteLine("ноль");
+}
+else
+{
+    Console.WriteLine("положительное");
+}
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 7;
+}
+CheckNumber(number);
+}
+}
