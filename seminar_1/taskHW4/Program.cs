55bb75273157ff6d3757988d59f81bbe7e2f5812
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 19:26:33 +0400

    create and wrote task4 and add gitignore

---
+++ seminar_1/taskHW4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вывести нечетные числа
+// Описание: Напишите метод, который на вход принимает число (number), а на выходе
+// выводит все нечетные числа от 1 до number (включительно), после каждого числа
+// должен быть знак пробела.
+
+
+public class Answer {
+static void PrintOddNumbers(int number)
+{
+// Введите свое решение ниже
+int count = 0;
+while(count< number)
+{
+    count++;
+    if (count % 2 == 1)
+    {
+        Console.Write($" {count}");
+    }
+}
+}
+static public void Main(string[] args) {
+int number;
+if (args.Length >= 1) {
+number = int.Parse(args[0]);
+} else {
+number = 5;
+}
+PrintOddNumbers(number);
+}
+}
