5643844c49647c762fa540589f97ff4329c521c7
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 14:21:04 +0400

    create tusk1 program and add gitignore

--- seminar_1/tusk1/Program.cs
+++ seminar_1/tusk1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+int numMax;
+int count;
+Console.Write("Введите целое положительное число: ");
+numMax = Convert.ToInt32(Console.ReadLine());
+count = numMax * -1;
+
+if (numMax>0)
+{
+ while(count<=numMax) 
+ {
+    Console.Write(count);
+    Console.Write(" ");
+    count++ ;
+ }
+}
+else
+{
+   Console.WriteLine("Ошибка ввода!");
+}
