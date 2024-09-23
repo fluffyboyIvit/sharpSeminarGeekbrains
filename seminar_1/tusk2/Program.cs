38134c6a8ca76949fbc5089169bcce61507cd832
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 15:36:15 +0400

    wrote a program tusk2

--- seminar_1/tusk2/Program.cs
+++ seminar_1/tusk2/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+int number;
+int fristDigit;
+int secondDigit;
+int sum;
+
+Console.Write("Введите трёхначиное положительное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (100 <= number && number <= 999)
+{
+    fristDigit = number/ 100;
+    secondDigit = number % 10;
+    sum = fristDigit + secondDigit;
+    Console.WriteLine($"Сумма первой и последней цифры числа {number} равна: {sum}");
+}
+else{
+    Console.WriteLine("Ошибка ввода!");
+
+}
