8d8b5a93a739fa340597cc85ab148018bf9f0d24
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-23 16:53:30 +0400

    create and wrote taskHW1 and add gitignore

---
+++ seminar_1/taskHW1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Описание: Напишите метод, который на вход принимает два целых числа и проверяет,
+// делится ли первое число на второе. Если делится, выводите "делится", иначе
+// выводите "не делится".
+
+
+public class Answer {
+static void CheckDivisibility(int firstNumber, int secondNumber)
+{
+// Введите свое решение ниже
+if(secondNumber != 0)
+{
+    if (firstNumber % secondNumber == 0)
+    {
+        Console.WriteLine("делится");
+    }
+    else
+    {
+        Console.WriteLine("не делится");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
+}
+static public void Main(string[] args) {
+int firstNumber, secondNumber;
+if (args.Length >= 2) {
+firstNumber = int.Parse(args[0]);
+secondNumber = int.Parse(args[1]);
+} else {
+firstNumber = 10;
+secondNumber = 2;
+}
+CheckDivisibility(firstNumber, secondNumber);
+}
+}
