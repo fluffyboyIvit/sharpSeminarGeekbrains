7fccb4a4534250c2b13ffb0c39558a347050d8f0
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-25 17:32:25 +0400

    add seminar2: tusk2

---
+++ seminar_2/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и возводит вторую цифру этого
+// числа в степень, равную третьей цифре
+
+
+
+int number;
+
+Console.Write("Введите трёзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int secondDigit = number / 10 % 10;
+    int lastDigit = number % 10;
+    int Count = 0;
+    int result = 1;
+    while (Count < lastDigit)
+    {
+        result = result * secondDigit;
+        Count++;
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
