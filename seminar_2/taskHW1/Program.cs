b93aae1a499abfc0c89797fb573f4e6bf359e16f
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-26 03:51:54 +0400

    add Seminar2: taskHW1

---
+++ seminar_2/taskHW1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.Write("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>0)
+{
+    if(number%7==0 && number%23==0)
+    {
+        Console.WriteLine("число кратно");
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно");
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
