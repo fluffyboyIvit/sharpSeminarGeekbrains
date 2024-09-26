ddb11119f668fb98217c3eb2956b62a444d579aa
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-26 04:26:40 +0400

    add seminar2:taskHW4

---
+++ seminar_2/taskHW4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if(number>0)
+{
+    while(count<=number)
+    {
+        Console.Write(count);
+        if(count<number)
+        {
+            Console.Write(",");
+        }
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
