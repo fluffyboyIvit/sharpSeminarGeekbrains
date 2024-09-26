6710d015b5f86469c1b03c0b616123c2490cb304
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-26 20:35:17 +0400

    created seminar_3: task1 and add gitignore

---
+++ seminar_3/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// // должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+int[] array = { 1, 3, 4, 19, 3 };
+int num = 8;
+bool isExistNum = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+string result = isExistNum ? "Да" : "Нет"; //тетрарный оператор
+Console.WriteLine(result);
