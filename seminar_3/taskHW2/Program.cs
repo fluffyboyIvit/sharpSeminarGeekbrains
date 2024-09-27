2644a4bad540e84083e67fb153c0e62a8c61a6de
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-27 15:25:44 +0400

    add seminar3: taskHW2

---
+++ seminar_3/taskHW2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Описание: Задайте массив из 10 элементов, заполненный числами от -10 до 10.
+// Замените отрицательные числа на их абсолютные значения, а положительные числа
+// на их отрицательные эквиваленты.
+class Program
+{
+static void SwapSigns(int[] numbers)
+{
+// Введите свое решение ниже
+int count = 0;
+ while(count < numbers.Length)
+ {  
+    if(numbers[count] != 0)
+    {
+      numbers[count] *= -1;  
+    }
+    
+    count++;
+ }
+}
+static void Main(string[] args)
+{
+int[] numbers = { 1, -5, 6, -7, 3 }; // Пример массива
+SwapSigns(numbers);
+Console.WriteLine("[" + string.Join(", ", numbers) + "]");
+}
+}
