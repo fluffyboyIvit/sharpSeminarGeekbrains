a2e87e27ca5ff830f16fed1f5f50fe07bfab471c
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-27 15:41:04 +0400

    add seminar3: taskHW3

---
+++ seminar_3/taskHW3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Описание: Задайте массив целых чисел. Напишите программу, которая проверяет,
+// присутствует ли заданное число в массиве. Программа должна вывести:
+// Присутствует/Не присутствует.
+
+class Program
+{
+static bool IsNumberPresent(int[] numbers, int numberToFind)
+{
+// Введите свое решение ниже
+bool isExistNum = false;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] == numberToFind)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+return isExistNum;
+ 
+}
+static void Main(string[] args)
+{
+int[] numbers = { 1, 3, 4, 19, 3 }; // Пример массива
+int numberToFind = 8; // Пример числа для поиска
+if (IsNumberPresent(numbers, numberToFind))
+{
+Console.WriteLine("Присутствует");
+}
+else
+{
+Console.WriteLine("Не присутствует");
+}
+}
+}
