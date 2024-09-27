3ac96c84d83b24e2f65f8abb903aeef60962c2e6
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-27 15:07:35 +0400

    add seminar3: taskHW1

---
+++ seminar_3/taskHW1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+// массива.
+
+class Program
+{
+static double CalculateAverage(int[] numbers)
+{
+// Введите свое решение ниже
+ double sum = 0;
+ int count = 0;
+ while(count<numbers.Length)
+ {
+    sum += numbers[count];
+    count++;
+ }
+ return sum / numbers.Length;
+
+}
+static void Main(string[] args)
+{
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Пример массива
+double average = CalculateAverage(numbers);
+Console.WriteLine(average);
+}
+}
