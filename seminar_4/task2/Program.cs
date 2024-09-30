8a3361e3b569f95bb98bb91981fa587143284a3b
fluffyboyIvit <1ibartysi1@gmail.com>
2024-09-30 15:21:13 +0400

    add semina4: task2

---
+++ seminar_4/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]
+// => 2
+
+
+int[] CreateRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+int CountNumsLastADevB(int[] array, int aLast, int bDev)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 10 == aLast && array[i] % bDev == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.Write("Введите размер массива: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateRndInt(sizeArr, 1, 100);
+PrintArray(arr);
+int result = CountNumsLastADevB(arr, 1, 7);
+Console.WriteLine($" => {result}");
