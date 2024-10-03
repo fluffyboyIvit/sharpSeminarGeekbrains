89868f105fbdcca556250d8a0bc2856c5a5c1d98
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-03 23:41:09 +0400

    remake seminar5:taskHW3

--- seminar_5/taskHW3/Program.cs
+++ seminar_5/taskHW3/Program.cs
@@ -12,31 +12,42 @@
 
 class Program
 {
-
-static void TransposeMatrix(int[,] array)
-{
-int rows = array.GetLength(0);
-int cols = array.GetLength(1);
-int[,] transposed = new int[cols, rows];
-// Введите свое решение ниже
-;
-for (int i = 0; i < rows; i++)
-{
-    for (int j = 0; j < cols; j++)
+static void PrintMatrix(int[,] matrix)
     {
-      transposed[j,i] += array[i,j] ;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.Write("|");
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j],4}");
+            }
+            Console.WriteLine(" |");
+        }
     }
-}
-
-}
-static void Main()
-{
-int[,] array =
-{
+static void TransposeMatrix(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        int[,] transposed = new int[cols, rows];
+        // Введите свое решение ниже
+        PrintMatrix(array);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                transposed[j, i] += array[i, j];
+            }
+        }
+        PrintMatrix(transposed);
+    }
+    static void Main()
+    {
+        int[,] array =
+        {
 { 1, 2 },
 { 3, 4 },
 { 5, 6 }
 };
-TransposeMatrix(array);
-}
+        TransposeMatrix(array);
+    }
 }
