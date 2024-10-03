eec74a7928c12eda031b63401d97c0d2cf66ccdf
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-03 14:12:13 +0400

    add seminar5: task5

---
+++ seminar_5/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и
+// т.д.
+
+
+
+int[,] CreateMatrixRndInt(int rows, int column, int min, int max)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[rows, column];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int SumMainDiogonalMatrix(int[,] matrix)
+{
+    
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, i];
+
+    }
+    return sum;
+}
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+int result = SumMainDiogonalMatrix(array2d);
+Console.WriteLine($" sum = {result}");
