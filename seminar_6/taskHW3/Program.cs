0ff716158cfa30836887145914e35394cd1ba284
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-05 19:50:01 +0400

    add seminar_6:taskHW3

---
+++ seminar_6/taskHW3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+bool StringIsPolyndrom (string str)
+{   
+    int  count = str.Length-1;
+
+    for (int i = 0; i < str.Length/2; i++)
+    {
+        if(str[i]!= str[count])
+        {   
+            return   false;
+        }
+        count--;
+    }
+    return true;
+}
+Console.WriteLine("Введите строку: ");
+string str = Console.ReadLine();
+
+bool polyndoromTrue = StringIsPolyndrom(str);
+string result = polyndoromTrue ? "Да": "Нет";
+Console.WriteLine(result);
