283a28e79ef581c05a34e2c987cd53bd4ae9bfec
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-05 17:52:19 +0400

    add seminar_6:taskHW2

---
+++ seminar_6/taskHW2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+string ReplacesUpperLettertoLower(string str)
+{   string strResult = string.Empty;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(char.IsUpper(str[i]))
+        {
+            strResult += char.ToLower(str[i]);
+        }
+        else
+        {
+            strResult += str[i];
+        }
+    }
+    return strResult;
+}
+
+Console.WriteLine("Введите строку с заглавными буквами: ");
+string str = Console.ReadLine();
+string lowerLine = ReplacesUpperLettertoLower(str);
+Console.WriteLine(lowerLine);
