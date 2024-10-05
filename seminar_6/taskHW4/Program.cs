c938e6ea2b8d919a4f682c478b7518b433d7a64f
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-06 01:28:05 +0400

    add seminar5: taskHw4

---
+++ seminar_6/taskHW4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+
+string InvertString(string str)
+{
+    string[] strNewSplit = str.Split();
+    string strInvert = "";
+    for (int i = strNewSplit.Length-1; i >= 0; i--)
+    {
+        strInvert += strNewSplit[i] + " ";
+    }
+    return strInvert;
+}
+Console.WriteLine("Введите предложение: ");
+string str = Console.ReadLine();
+string result = InvertString(str);
+Console.WriteLine(result);
