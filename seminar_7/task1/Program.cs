4b4612b4cc8e9f31bcb81aeb4ab4d0df972bb410
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-06 17:29:54 +0400

    create seminar_7:task1

---
+++ seminar_7/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+
+
+
+void PrintNaturalNum (int num)
+{
+    if(num != 1)
+    {   
+    PrintNaturalNum(num-1);
+    }
+    Console.WriteLine(num);
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNum(number);
