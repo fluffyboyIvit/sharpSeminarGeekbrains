65d61af614fc60ff5e8ccc02d463eeb4fd1d7195
fluffyboyIvit <1ibartysi1@gmail.com>
2024-10-08 14:29:02 +0400

    add seminar_7: task2

---
+++ seminar_7/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию
+int SumDigitNumber(int num)
+{
+    
+    if(num==0)
+    {
+        return 0;
+    }
+    
+    return  num % 10 + SumDigitNumber(num/10) ;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumDigitNumber(number);
+Console.WriteLine(result);
